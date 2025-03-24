f0e3d5576414d08df77e06a29a59f5d04c661853
sayantan007pal <sayantanpal100@gmail.com>
2025-03-24 16:18:12 +0530

    post - return borrowed book and fine payble is only left to resolve

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/userController.js');
 
 // GET all users or search with filters
 router.get('/', userController.getAllUsers);
